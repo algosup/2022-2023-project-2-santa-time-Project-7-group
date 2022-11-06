b27804c7de1d8f87feb682a3aa3e1f770cff4190
BERNARD Max <80251657+maxbernard3@users.noreply.github.com>
2022-11-06 01:58:28 +0100

    setup calendar with date check
    can't see content too early

--- Santa/static/js/calendar.js
+++ Santa/static/js/calendar.js
@@ -1,6 +1,7 @@
 var calendar = document.getElementById("popup_calendar");
 var btn_in = document.getElementById("calendar_btn");
 var btn_out = document.getElementById("close_popup");
+var img_out = document.getElementById("close_img");
 var grid = document.getElementById("grid");
 
 btn_out.onclick = function () {
@@ -9,36 +10,45 @@ btn_out.onclick = function () {
 
 btn_in.onclick = function () {
     calendar.style.display = "block";
-    div = document.getElementById("div_id1")
+    div = document.getElementById("div_id1");
     if (div == 'undefined' || div == null){
-        div24()
+        div24();
     }
 };
 
+function bigImage (i) {
+    //const d = new Date();
+    let date = new Date();
+    let d = date.getDate();
+    if(i <= d){
+        document.getElementById("big_image").style.display = "flex";
+        document.getElementById("denter_grid").style.display = "none";
+        document.getElementById("close_popup").style.display = "none";
+    }
+    else {
+        document.getElementById("tooSoon").style.display = "block";
+        document.getElementById("setText").innerHTML = `${i - d} days too soon`;
+    }
+}
 
+document.getElementById("tooSoon").onclick = function() {
+    document.getElementById("tooSoon").style.display = "none";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+img_out.onclick = function () {
+    document.getElementById("big_image").style.display = "none";
+    document.getElementById("denter_grid").style.display = "block";
+    document.getElementById("close_popup").style.display = "block";
+}
 
 function div24() {
-    var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-    var arr = new shuffleArray(arr1)
+    var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+    var arr = new shuffleArray(arr1);
     for (let i = 0; i < arr.length; i++) {
         let div = document.createElement("div");    
-        div.innerHTML = arr[i]
+        div.innerHTML = arr[i];
         div.id = `div_id${arr[i]}`;
+        div.setAttribute("onclick" , `bigImage(${arr[i]})`);
         grid.appendChild(div);
     }
 }
