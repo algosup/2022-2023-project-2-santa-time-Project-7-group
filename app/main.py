9d4033d47d8841f9472d4d0f7e5e3d0b28e4266b
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-11-18 11:18:52 +0100

    data generation

--- app/main.py
+++ app/main.py
@@ -1,20 +1,53 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import multiprocessing
+from multiprocessing import Pool
+import random
 
-options = Options()
+def remove_unecesary_lines():
+    r = open("../adress.txt", 'r')
+    contents = r.readlines()
+    r.close()
 
-service = Service(executable_path="/app/chromedriver")
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-gpu")
+    w = open("../adress.txt", 'w')
+    for line in contents:
+        if not line == '\n':
+            w.write(line)
+    w.close
 
+def uplow_case_randomizer(address):
+    s = list(address)
+    for i in range(random.randint(0, (len(address) - 1))):
+        n = random.randint(0, (len(address) - 1))
+        if not s[n].isupper():
+            s[n] = s[n].upper()
+        else:
+            s[n] = s[n].lower()
+    return("".join(s))       
 
-driver = webdriver.Chrome(service=service, options=options)
+def remove_info(address):
+    s = address.split(", ")
+    for i in range(random.randint(0, (len(s) - 1))):
+        n = random.randint(0, (len(address) - 1))
+def process(line):
+    print("i")
 
-driver.get("https://fr.wikipedia.org/wiki/Mozilla_Firefox")
-text = driver.find_element(By.CLASS_NAME, "mw-page-title-main")
+# Reserve cores
+def multi_process():
+    # alocate CPU
+    max_processors = multiprocessing.cpu_count()*4
+    pool = Pool(processes=max_processors)
+    f_list = []
 
-print(text)
+    for i in range(64):
+        # pass the df to process function
+        f = pool.apply_async(process, [i])
+        f_list.append(f)
+
+        # safe gard from OOM
+        if len(f_list) == max_processors:
+            for f in f_list:
+                f.get()
+                del f_list[:]
+
+if __name__ == "__main__":
+    a = uplow_case_randomizer("0956 Rath Loaf, Apt. 811, 13224, Janiceborough, Arizona, United States")
+    print(a)
