caad456d551316ebee04aed7857ad9783afd99af
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-11-24 11:48:19 +0100

    Request added

--- Santa/static/js/maths.js
+++ Santa/static/js/maths.js
@@ -30,4 +30,40 @@ function calcSantaTime(loc, Z){
     return [hours(lt), minutes(lt), seconds(lt)];
 }
 
-console.log(calcSantaTime(2.0698, 1));
+// console.log(calcSantaTime(2.0698, 1));
+
+function getAnswer(){
+    //use nominatim api to get lat and long
+    var url = "https://nominatim.openstreetmap.org/search?q=" + document.getElementById("location").value +"+"+document.getElementById("country").title + "&format=json&polygon=1&addressdetails=1";
+    console.log(url);
+    //fetch the data
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.length == 0){
+            document.getElementById("answer").innerText = "Invalid location";
+            return;
+        }
+        //get the lat and long
+        var lat = data[0].lat;
+        var long = data[0].lon;
+        //get the time zone
+        var url = "https://fastapi.geogarage.com/timezone/?lat=" + lat + "&lon=" + long + "&key=6d4558cef22ce8b18718f88b233f6d72a7c4ed8b";
+        //cors policy 
+        fetch(url, {mode: 'no-cors'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            //get utc offset
+            var Z = data.utc_offset;
+            var final = calcSantaTime(lat, Z);
+            //write to answer id="time"
+            document.getElementById("answer").innerText = final[0] + ":" + final[1] + ":" + final[2];
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    .catch(err => console.warn(err.message));
+    })
+}
