4b0dd3b986166c0bdb805be575529204efbbe8c0
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-11-25 17:31:38 +0100

    Port 80 handling temporary

---
+++ SantaPORT80/Requests.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+)
+
+//go:embed static
+var content embed.FS
+
+func handler() http.Handler {
+
+	fsys := fs.FS(content)
+	html, _ := fs.Sub(fsys, "static")
+
+	return http.FileServer(http.FS(html))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.Handle("/", handler())
+
+	http.ListenAndServe(":80", mux)
+	// http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/catchyoursanta.ml/fullchain.pem", "/etc/letsencrypt/live/catchyoursanta.ml/privkey.pem", mux)
+}
