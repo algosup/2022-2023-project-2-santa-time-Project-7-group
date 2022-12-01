70b1518df0f88e263d79849d2f0d976ea608ec13
Molnar Ivan <80251702+ivan-molnar@users.noreply.github.com>
2022-12-01 10:39:46 +0100

    Added day verification + bugfix

--- Santa/static/js/gacha.js
+++ Santa/static/js/gacha.js
@@ -2,6 +2,26 @@ function gacha(day){
     let rand = Math.random();
 
     for(let i=gacha_chances.length-1; i>-1; i--){
-        if(rand>gacha_chances[i][0]) return gacha_chances[i][1][day];
+        if(rand>gacha_chances[i][0]){
+            if (localStorage.getItem("santa_gachas") === null) {
+                localStorage.setItem("santa_gachas", JSON.stringify([[day, gacha_chances[i][1][day-1]]]));
+            }else{
+                let santa_gachas = JSON.parse(localStorage.getItem("santa_gachas"));
+                santa_gachas.push([day, gacha_chances[i][1][day-1]]);
+
+                localStorage.setItem("santa_gachas", JSON.stringify(santa_gachas));
+
+            }
+
+            return gacha_chances[i][1][day-1];
+        }
     }
+}
+
+function verifGachaHistory(day){
+    let santa_gachas = JSON.parse(localStorage.getItem("santa_gachas"));
+    if(santa_gachas === null 
+    || santa_gachas.filter(e => e[0] == day).length == 0) return true;
+
+    return false;
 }
