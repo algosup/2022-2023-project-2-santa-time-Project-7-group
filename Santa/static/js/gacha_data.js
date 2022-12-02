df7ee052d1e34bff3c78b2368f29241f69290708
Molnar Ivan <80251702+ivan-molnar@users.noreply.github.com>
2022-12-02 12:13:51 +0100

    added day 3 gachas

--- Santa/static/js/gacha_data.js
+++ Santa/static/js/gacha_data.js
@@ -1,7 +1,7 @@
-let gachas_common = ["c1.png", "c2.png"];
-let gachas_rare = ["r1.png", "r2.png"];
-let gachas_epic = ["e1.png", "e2.png"];
-let gachas_legendary = ["l1.png", "l2.png"];
-let gachas_mythic = ["m1.png", "m2.png"];
+let gachas_common = ["c1.png", "c2.png", "c3.png"];
+let gachas_rare = ["r1.png", "r2.png", "r3.png"];
+let gachas_epic = ["e1.png", "e2.png", "e3.png"];
+let gachas_legendary = ["l1.png", "l2.png", "l3.png"];
+let gachas_mythic = ["m1.png", "m2.png", "m3.png"];
 
 let gacha_chances = [[0, gachas_common], [0.4, gachas_rare], [0.7, gachas_epic], [0.9, gachas_legendary], [0.98, gachas_mythic]];
