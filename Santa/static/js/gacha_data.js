e3dac9871e81adb9f5ce6935aa2e1d07b99ca634
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-12-06 04:42:06 +1300

    image up to 7days

--- Santa/static/js/gacha_data.js
+++ Santa/static/js/gacha_data.js
@@ -1,7 +1,7 @@
-let gachas_common = ["c1.png", "c2.png", "c3.png", "c4.png", "c5.png", "c6.png"];
-let gachas_rare = ["r1.png", "r2.png", "r3.png", "r4.png", "r5.png", "r6.png"];
-let gachas_epic = ["e1.png", "e2.png", "e3.png", "e4.png", "e5.png"];
-let gachas_legendary = ["l1.png", "l2.png", "l3.png", "l4.png", "l5.png", "l6.png"];
-let gachas_mythic = ["m1.png", "m2.png", "m3.png", "m4.png", "m5.png", ];
+let gachas_common = ["c1.png", "c2.png", "c3.png", "c4.png", "c5.png", "c6.png", "c7.png", "c8.png"];
+let gachas_rare = ["r1.png", "r2.png", "r3.png", "r4.png", "r5.png", "r6.png", "r7.png"];
+let gachas_epic = ["e1.png", "e2.png", "e3.png", "e4.png", "e5.png", "e6.png", "e7.png"];
+let gachas_legendary = ["l1.png", "l2.png", "l3.png", "l4.png", "l5.png", "l6.png", "l7.png"];
+let gachas_mythic = ["m1.png", "m2.png", "m3.png", "m4.png", "m5.png", "m6.png", "m7.png"];
 
 let gacha_chances = [[0, gachas_common], [0.4, gachas_rare], [0.7, gachas_epic], [0.9, gachas_legendary], [0.98, gachas_mythic]];
