7fbbcfaf08b6412a395ac6474563183069173735
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-12-12 15:46:16 +0100

    data analysis added

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require golang.org/x/crypto v0.3.0
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 )
--- serverDB/main.go
+++ serverDB/main.go
@@ -4,7 +4,101 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var (
+	//map of file names and their hash
+	hashes map[string]string = map[string]string{"lerihcoizhuogeippajidei": "lkdncounter.txt", "ppaubdcizbdevixybgz": "redditcounter.txt", "ceoyabcuzydevbaixdyeuihrbx": "discordcounter.txt", "apzkeiobhixcbziugbdvuf": "twittercounter.txt", "randvisitorsiuezbci": "randvisitors.txt"}
+	lkdn   int
+	redd   int
+	disc   int
+	twit   int
+	randv  int
+)
+
+// function for text file that increments the counter
+func counters(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	//check url size if > 150
+	if len(r.URL.Path) > 150 {
+		http.Error(w, "URL too long", http.StatusBadRequest)
+		return
+	}
+	switch r.URL.Path {
+	case "/lerihcoizhuogeippajidei":
+		lkdn++
+		randv++
+	case "/ppaubdcizbdevixybgz":
+		redd++
+		randv++
+	case "/ceoyabcuzydevbaixdyeuihrbx":
+		disc++
+		randv++
+	case "/apzkeiobhixcbziugbdvuf":
+		twit++
+		randv++
+	case "randvisitorsiuezbci":
+		randv++
+	}
+}
+
+func saveVarsToFiles() {
+	// open or create the file matching the hash
+	for k, v := range hashes {
+		file, err := os.OpenFile(v, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		switch k {
+		case "lerihcoizhuogeippajidei":
+			file.WriteString(strconv.Itoa(lkdn))
+		case "ppaubdcizbdevixybgz":
+			file.WriteString(strconv.Itoa(redd))
+		case "ceoyabcuzydevbaixdyeuihrbx":
+			file.WriteString(strconv.Itoa(disc))
+		case "apzkeiobhixcbziugbdvuf":
+			file.WriteString(strconv.Itoa(twit))
+		case "randvisitorsiuezbci":
+			file.WriteString(strconv.Itoa(randv + lkdn + redd + disc + twit))
+		}
+	}
+}
+
+func setVarsFromFiles() {
+	// open or create the file matching the hash
+	for k, v := range hashes {
+		file, err := os.OpenFile(v, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		body, _ := ioutil.ReadAll(file)
+		switch k {
+		case "lerihcoizhuogeippajidei":
+			lkdn, _ = strconv.Atoi(string(body))
+		case "ppaubdcizbdevixybgz":
+			redd, _ = strconv.Atoi(string(body))
+		case "ceoyabcuzydevbaixdyeuihrbx":
+			disc, _ = strconv.Atoi(string(body))
+		case "apzkeiobhixcbziugbdvuf":
+			twit, _ = strconv.Atoi(string(body))
+		case "randvisitorsiuezbci":
+			randv, _ = strconv.Atoi(string(body))
+		}
+	}
+}
+
+func getStatus(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	//return all the counters
+	w.Write([]byte("LinkedIn: " + strconv.Itoa(lkdn) + " Reddit: " + strconv.Itoa(redd) + " Discord: " + strconv.Itoa(disc) + " Twitter: " + strconv.Itoa(twit) + " TOTAL: " + strconv.Itoa(randv)))
+}
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
@@ -71,9 +165,23 @@ func Handlertz(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//call saveVarsToFiles every 1hour
+	go func() {
+		for {
+			setVarsFromFiles()
+			saveVarsToFiles()
+			time.Sleep(1 * time.Hour)
+		}
+	}()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api", Handler)
 	mux.HandleFunc("/tz/", Handlertz)
+	mux.HandleFunc("/lerihcoizhuogeippajidei", counters)
+	mux.HandleFunc("/ppaubdcizbdevixybgz", counters)
+	mux.HandleFunc("/ceoyabcuzydevbaixdyeuihrbx", counters)
+	mux.HandleFunc("/apzkeiobhixcbziugbdvuf", counters)
+	mux.HandleFunc("/randvisitorsiuezbci", counters)
+	mux.HandleFunc("/stats", getStatus)
 
 	redirect := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "https://noel.gq"+r.RequestURI, http.StatusMovedPermanently)
