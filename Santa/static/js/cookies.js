84605fcc68fb266b481d662d71d99b1fbe88d9cd
David Cuahonte Cuevas <91249658+DavidCC812@users.noreply.github.com>
2022-12-14 12:21:21 +0100

    Cookies disappear

--- Santa/static/js/cookies.js
+++ Santa/static/js/cookies.js
@@ -1,4 +1,7 @@
-function myFunction() {
-          console.log("hi");
+function cookieHandler() {
+          document.getElementById("cookie").style.display = "none";
+          localStorage.setItem("cookie_accepted", "true");
+}
+if (localStorage.getItem("cookie_accepted") == "true") {
           document.getElementById("cookie").style.display = "none";
 }
